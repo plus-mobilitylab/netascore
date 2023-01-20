ce26ee91a7ab1dad19ef89631bbf6992e088dc18
Christian Werner <mail.christian-werner@web.de>
2023-01-20 11:46:06 +0100

    added warning when providing DEM with GIP

--- core/attributes_step.py
+++ core/attributes_step.py
@@ -42,6 +42,8 @@ class GipAttributesStep(DbStep):
                 'table_greenness': db.use_if_exists('greenness', self.db_settings.entities.data_schema),
                 'table_water': db.use_if_exists('water', self.db_settings.entities.data_schema)
             }
+            if params.table_dem is not None:
+                h.majorInfo("WARNING: You provided a DEM file. However, for GIP attribute calculation only the elevation data contained in the GIP dataset is used. Your provided DEM is ignored.")
             db.execute_template_sql_from_file("gip_attributes", params)
             db.commit()
         h.logEndTask()
